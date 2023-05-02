10698321f2b9092163c468ff966b2a1dd8d8fd37
dabrynskiy <dabrynskiy@gmail.com>
2023-05-03 00:34:30 +0300

    test handle api get

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,9 @@ const app = express();
 app.listen(PORT, () => {
     console.log('Server starting');
 });
+
+app.get('/api/hello', (request, response) => {
+    response.json(
+        { greeting: "hello from api" }
+    );
+});
