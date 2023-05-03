f0614042a19192e3cf919829370b00b1ce628360
dabrynskiy <dabrynskiy@gmail.com>
2023-05-03 13:11:57 +0300

    experiments with API and use postman

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log('Server starting');
@@ -10,6 +15,12 @@ app.listen(PORT, () => {
 
 app.get('/api/hello', (request, response) => {
     response.json(
-        { greeting: "hello from api" }
+        { get: "Hello get request!" }
     );
 });
+
+app.post('/api/hello', (request, response) => {
+    response.json(
+        { post: "Hello post request!", requestBody: request.body }
+    )
+})
